82cfc41e867f85aa9336dca21f8ae3b6bc4435cd
Aleksey-Smouzh <xmz@tut.by>
2022-02-21 15:35:42 +0300

    fixed TodoList

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,20 +1,15 @@
+import "./App.css";
 
-
-import './App.css';
-// import TodoForm from './components/TodoForm';
-import TodoList from './components/TodoList';
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
-  <>
-  <div className="todo_app">
-  {/* <TodoForm/> */}
-  <TodoList/>
-
-
-  </div>
-  
-  </>)
+    <>
+      <div className="todo_app">
+        <TodoList />
+      </div>
+    </>
+  );
 }
 
 export default App;
