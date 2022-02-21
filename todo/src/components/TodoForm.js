3dd5662bd748d026b05e972cd6fc653233f57a6b
Aleksey-Smouzh <xmz@tut.by>
2022-02-21 10:54:56 +0300

    created comp TodoList

--- todo/src/components/TodoForm.js
+++ todo/src/components/TodoForm.js
@@ -3,18 +3,18 @@ import React, { useState } from "react";
 function TodoForm(props) {
   const [input, setInput] = useState("");
 const handleChange = e => {
-    setInput(e.target.value)
-}
+    setInput(e.target.value);
+};
 
 
 
   const hendleSubmit = e => {
       e.preventDefault();
 
-//  props.onSubmit({
-//      id: Math.floor(Math.rendom() * 10000),
-//      text: input
-//  })
+ props.onSubmit({
+     id: Math.floor(Math.random() * 10000),
+     text: input
+ })
 setInput('');
   };
   return (
